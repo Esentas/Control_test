bf4a497a123a77cf19288e765ade4435fc050fdf
esentas <bo.evoi@yandex.ru>
2022-08-10 11:05:12 +0500

    3 commit

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[]{
+        "run", "black", "green", "gray", "true", "false", "yes",
+        "brown", "world", "no", "geek", "brains", "world"};
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+void SelectThreeSymbol(string[] array, int count) {
+    int lenOfarray = array.Length;
+    string[] arraySelect = new string[lenOfarray];
+    int countOfString = 0;
+
+    for(int i = 0; i < lenOfarray; i++) {
+        if(array[i].Length <= count) {
+            arraySelect[countOfString] = array[i];
+            countOfString++;
+        }
+    }
+    Array.Resize(ref arraySelect, countOfString);
+    Console.WriteLine("[" + string.Join(", ", arraySelect) + "]");
+}
+
+SelectThreeSymbol(array, 3);
